c00009b6b9a6373f798ed0e6df93d9e07eb872d0
Akul Kapoor <akulkapoor@gmail.com>
2012-10-04 14:25:48 -0400

    Bio

--- BackBeat/app2.js
+++ BackBeat/app2.js
@@ -7,20 +7,47 @@ onReady = function() {
 		$('#picture').html('')
 		var big = $(this).attr("data-big");
 		var band = $(this).attr("band");
+		var object = $(this);
 		$('#picture').append(band +"<br>");
 		$('#picture').append("<img src = " + big + ">");
 		$("#picture").attr('class', 'show');
+		setInfo(object,band);
 	});
 	$('.link').live("click",function(){
 		$('#picture').html('')
-		console.log(this);
 		var big = $(this.innerHTML).attr("data-big");
 		var band = $(this.innerHTML).attr("band");
+		var object = $(this.innerHTML);
 		$('#picture').append(band +"<br>");
 		$('#picture').append("<img src = " + big + ">");
 		$("#picture").attr('class', 'show');
+		setInfo(object,band);
 	});
 }
+
+setInfo = function(object,band) {
+	$.getJSON('http://ws.audioscrobbler.com/2.0/',
+			{
+				method: "artist.getInfo",
+				api_key: "8319d81dde2f49bad5c65a0ce2361a31",
+				format: "json",
+				artist: band,
+				limit: 250
+			},
+			function(data) {
+				var info = document.createElement("div");
+				info.className = "info";
+				if (data.artist  === undefined) {
+					info.innerHTML = object.attr("city") + "<br>" + object.attr("country") + "<br>" + object.attr("theatre");
+				}
+				
+				else {
+				info.innerHTML = data.artist.bio.content;
+				}
+				$("#picture").append(info);
+			});	
+}
+
 		doSearch = function() {
 			$('#picture').html('')
 			simArts();
@@ -47,6 +74,7 @@ var data1;
 			function(data) {
 				data1 = data;
 				$("#results3").html("");
+				console.log(data);
 
 
 				$.each(data1.events.event, function(i, item) {
@@ -58,17 +86,18 @@ var data1;
 
 						var img = document.createElement("div");
 						img.className = "img";
-						img.innerHTML = "<img src=" + item.image[3]["#text"] + " data-big=" + item.image[3]["#text"] + " band=" + item.artists.artist + ">"
+						img.innerHTML = "<img src=" + item.image[3]["#text"] + " data-big=" + item.image[3]["#text"] + " band=" + item.artists.artist + " city='" + item.venue.location.city + "'" + " country='" + item.venue.location.country + "'" + " theatre='" + item.venue.name + "'>"
 
 						var link = document.createElement("div");
 						link.className = "link";
-						if (item.url.slice(0,7) !== "http://") {
+						/*if (item.url.slice(0,7) !== "http://") {
 							link.innerHTML = "<a href='" + "http://" + item.url + "'>" + item.artists.artist + "</a>";
 							//link.innerHTML = "<a href='" + "http://" + item.url + "'>" + item.artists.artist + "</a>";
 						}
 						else {
 							link.innerHTML = "<a href='" + item.url + "'>" + item.artists.artist + "</a>";
-						}
+						}*/
+						link.innerHTML = "<div id='" + item.artists.artist  + "' data-big=" + item.image[3]["#text"] + " band='" + item.artists.artist + "'' city='" + item.venue.location.city + "'" + " country='" + item.venue.location.country + "'" + " theatre='" + item.venue.name+ "'>" + item.artists.artist + "</div>";
 						artist.appendChild(link);
 						artist.appendChild(img);
 						artist.innerHTML += "<br>"
@@ -179,13 +208,14 @@ var data2;
 
 						var link = document.createElement("div");
 						link.className = "link";
-						if (item.url.slice(0,7) !== "http://") {
+						/*if (item.url.slice(0,7) !== "http://") {
 							link.innerHTML = "<a href='" + item.url + " data-big=" + item.image[4]["#text"] + " band='" + item.name + "'>" + item.name + "</a>";
 							//link.innerHTML = "<a href='" + "http://" + item.url + "'>" + item.name + "</a>";
 						}
 						else {
 							link.innerHTML = "<a href='" + item.url + " data-big=" + item.image[4]["#text"] + " band='" + item.name + "'>" + item.name + "</a>";
-						}
+						}*/
+						link.innerHTML = "<div id='" + item.name  + "' data-big=" + item.image[4]["#text"] + " band='" + item.name + "'>" + item.name + "</div>";
 						artist.appendChild(link);
 						artist.appendChild(img);
 						artist.innerHTML += "<br>"
